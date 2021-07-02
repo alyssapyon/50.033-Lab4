b366446e8141661ed7d84b4d2062bf459878525b
alyssapyon <50566448+alyssapyon@users.noreply.github.com>
2021-07-02 11:59:43 +0800

    i think im done?

---
+++ Mario Recreate 2019 ver/Assets/Scripts/OrangeMushroom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// SPEED BOOST
+public class OrangeMushroom : MonoBehaviour, ConsumableInterface
+{
+	public Texture t;
+	public void consumedBy(GameObject player)
+	{
+		// give player jump boost
+		player.GetComponent<PlayerController>().speedBoost *= 2;
+		StartCoroutine(removeEffect(player));
+	}
+
+	IEnumerator removeEffect(GameObject player)
+	{
+		yield return new WaitForSeconds(5.0f);
+		player.GetComponent<PlayerController>().speedBoost /= 2;
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.CompareTag("Player"))
+		{
+			// update UI
+			CentralManager.centralManagerInstance.addPowerup(t, 1, this);
+			GetComponent<Collider2D>().enabled = false;
+		}
+	}
+
+
+}
---
+++ Mario Recreate 2019 ver/Assets/Scripts/RedMushroom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// JUMP BOOST
+
+public class RedMushroom : MonoBehaviour, ConsumableInterface
+{
+	public Texture t;
+	public void consumedBy(GameObject player)
+	{
+		// give player jump boost
+		player.GetComponent<PlayerController>().jumpBoost += 10;
+		StartCoroutine(removeEffect(player));
+	}
+
+	IEnumerator removeEffect(GameObject player)
+	{
+		yield return new WaitForSeconds(5.0f);
+		player.GetComponent<PlayerController>().jumpBoost -= 10;
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.CompareTag("Player"))
+		{
+			// update UI
+			CentralManager.centralManagerInstance.addPowerup(t, 0, this);
+			GetComponent<Collider2D>().enabled = false;
+		}
+	}
+
+
+}
